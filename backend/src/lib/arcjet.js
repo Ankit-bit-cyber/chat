83c6c03a00b6988bd012d4b79c76784cf1438dfc
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-01-15 22:31:02 +0530

    First commit

---
+++ backend/src/lib/arcjet.js
@@ -0,0 +1,28 @@
+import arcjet, { shield, detectBot, slidingWindow } from "@arcjet/node";
+
+import { ENV } from "./env.js";
+
+const aj = arcjet({
+  key: ENV.ARCJET_KEY,
+  rules: [
+    // Shield protects your app from common attacks e.g. SQL injection
+    shield({ mode: "LIVE" }),
+    // Create a bot detection rule
+    detectBot({
+      mode: "LIVE", 
+      
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", 
+       
+      ],
+    }),
+ 
+    slidingWindow({
+      mode: "LIVE", 
+      max: 100,
+      interval: 60,
+    }),
+  ],
+});
+
+export default aj;
