7f36d8b18f3bd0895c13b0c9dd86c7cb9fd89212
Ankit-bit-cyber <208ankitkumarssm@gmail.com>
2026-05-30 12:33:27 +0530

    fix: update axios baseURL for development environment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:  import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL:  import.meta.env.MODE === "development" ? "https://chat-49y9.vercel.app/api" : "/api",
   withCredentials: true,
 });
